d80390b4354befe55802e292122b8059462c1b7d
Stanislav Sysoev <d4rkr00t@gmail.com>
2017-09-01 18:15:58 +1000

    Make it browser only

--- src/main.js
+++ src/main.js
@@ -47,19 +47,5 @@ exports.clone = function() {
   return defaultInstance.clone.apply(defaultInstance, arguments);
 };
 
-
-if (environment.isBrowser) {
-  exports.homepage = '{{package-homepage}}';
-  exports.version = '{{package-version}}';
-} else {
-  var packageInfoModuleName = '../package.json';
-  var packageInfo = require(packageInfoModuleName);
-  exports.homepage = packageInfo.homepage;
-  exports.version = packageInfo.version;
-
-  var formatterModuleName = './formatters';
-  var formatters = require(formatterModuleName);
-  exports.formatters = formatters;
-  // shortcut for console
-  exports.console = formatters.console;
-}
+exports.homepage = "{{package-homepage}}";
+exports.version = "{{package-version}}";
